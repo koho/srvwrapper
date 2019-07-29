4a80db94e4aaa4c5d66696c7816178d9a6d71626
gerhard <gerhard.gh.ta@gmail.com>
2019-07-29 14:34:24 +0800

    Add long description

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
+import os
+
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def readall(*args):
+    with open(os.path.join(here, *args), encoding='utf8') as fp:
+        return fp.read()
+
+
+README = readall('README.md')
 
 setup(
     name='srvwrapper',
     version='1.0',
     description='srvwrapper wraps any applications to run as Windows Service',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='Gerhard Tan',
     author_email='gerhard.gh.ta@gmail.com',
     url='https://github.com/koho/srvwrapper',
